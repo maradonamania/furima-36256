b23a3f5fd76f30ba3449f9ca7e8db7f23258fd05
maradonamania <80200804+maradonamania@users.noreply.github.com>
2021-07-28 23:48:07 +0900

    削除機能の追加とmove_to_indexメソッドの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
-  before_action :set_item, only:[:show,:edit,:update]
-  before_action :move_to_index, except: [:index,:show]
+  before_action :set_item, only:[:show,:edit,:update,:destory]
+  before_action :move_to_index, only: [:edit,:destroy]
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -24,7 +24,11 @@ class ItemsController < ApplicationController
   end
 
   def edit
+  end
 
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   def update
